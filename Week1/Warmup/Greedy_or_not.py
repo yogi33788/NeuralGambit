f8d74e2cfba374f2fccef11d39302af50cb15e11
yogeswar <yogeswar9573@gmail.com>
2026-05-22 10:35:34 +0530

    Greedy_or_not.py added only 7 testcases passed

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,1 +1,19 @@
-# Write your code here
+def p_score(a):
+    if len(a) == 0:
+        return 0
+    add = sum(a)
+    score1 = add - p_score(a[1:])
+    score2 = add - p_score(a[:-1])
+    return max(score1, score2)
+
+n = int(input())
+a = list(map(int, input().split()))
+tot = sum(a)
+p1 = p_score(a)
+p2 = tot - p1
+if p1 > p2:
+    print("Player 1 wins")
+elif p1 < p2:
+    print("Player 2 wins")
+else:
+    print("Its a draw")
